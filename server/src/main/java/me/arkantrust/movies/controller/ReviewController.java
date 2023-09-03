04a2870a6291ffe9308135bef006374262adb7c1
arkantrust <davidulce26@gmail.com>
2023-09-03 00:39:31 -0500

    .

--- server/src/main/java/me/arkantrust/movies/controller/ReviewController.java
+++ server/src/main/java/me/arkantrust/movies/controller/ReviewController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import me.arkantrust.movies.Service.ReviewService;
 import me.arkantrust.movies.model.Review;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
